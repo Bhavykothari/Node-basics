1967922cfc6d11bcdf3f1288584a8171264e949d
Bhavy <Bhavy@gmail.com>
2023-06-07 19:13:38 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 ;
 
 app.get('/hello', (req, res) => {
-  res.send('The API reference documentation provides detailed information about a function or object in Node.js. This documentation indicates what arguments a method accepts, the return value of that method, and what errors may be related to that method. It also indicates which methods are available for different versions of Node.js.')
+  res.send('The Application programming interface reference documentation provides detailed information about a function or object in Node.js. This documentation indicates what arguments a method accepts, the return value of that method, and what errors may be related to that method. It also indicates which methods are available for different versions of Node.js.')
 })
 
 app.listen(port, () => {
