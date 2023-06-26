dd16ac66ca3e92de596bfeda0cca004fe2215ea7
Bhavy <Bhavy@gmail.com>
2023-06-26 17:26:48 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 5000 ;
 
-app.get('/hello', (req, res) => {
-  res.send('Total new code')
+app.get('/', (req, res) => {
+  res.send('Node application running')
 })
 
 app.listen(port, () => {
