79d57ef58018296c33029809677811be846c145d
Bhavykothari <84842331+Bhavykothari@users.noreply.github.com>
2023-06-26 17:47:29 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app2">
-      <pre>
-      A reverse proxy is the recommended method to expose an application server to the internet. Whether you are running a Node.js application in production or a minimal built-in web server with Flask, these application servers will often bind to localhost with a TCP port. This means by default, your application will only be accessible locally on the machine it resides on. While you can specify a different bind point to force access through the internet, these application servers are designed to be served from behind a reverse proxy in production environments. This provides security benefits in isolating the application server from direct internet access, the ability to centralize firewall protection, and a minimized attack plane for common threats such as denial of service attacks.
-
-From a client’s perspective, interacting with a reverse proxy is no different from interacting with the application server directly. It is functionally the same, and the client cannot tell the difference. A client requests a resource and then receives it, without any extra configuration required by the client.
-
-This tutorial will demonstrate how to set up a reverse proxy using Nginx, a popular web server and reverse proxy solution. You will install Nginx, configure it as a reverse proxy using the proxy_pass directive, and forward the appropriate headers from your client’s request. If you don’t have an application server on hand to test, you will optionally set up a test application with the WSGI server Gunicorn.
-
-
-      </pre>
+    <div className="App"> 
+      <div>" React server is started with Nginx proxy server "</div>
     </div>
   );
 }
