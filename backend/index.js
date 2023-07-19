ca6b0ba4fb1f61a772e9f7e5f4c1e701868e52be
Bhavy kothari <84842331+datsBhavy@users.noreply.github.com>
2023-07-19 16:15:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000 ;
 
 app.get('/', (req, res) => {
-  res.send('Node application running on 4000....................................')
+  res.send('This is the testing website for node.js')
 })
 
 app.listen(port, () => {
